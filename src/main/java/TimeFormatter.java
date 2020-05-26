9fab79da69486eea06b6c2491848b14d3e938ddf
Emma Jones <emma.byrne@johnlewis.co.uk>
2020-05-26 18:48:24 +0100

    working for all time units, needs refactoring

--- src/main/java/TimeFormatter.java
+++ src/main/java/TimeFormatter.java
@@ -1,10 +1,100 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class TimeFormatter {
-    public static String convertTime(Integer seconds) {
-        return (seconds <= 0) ? "none" : calculateTime(seconds);
+
+    public enum UnitOfTime {
+
+        second(1), minute(60), hour(3600), day(86400), year(31536000);
+
+        private int secs;
+
+        UnitOfTime(int secs) {
+            this.secs = secs;
+        }
     }
 
-    private static String calculateTime(Integer seconds) {
-        return seconds.toString() + " seconds";
+    public static String convertTime(int secs) {
+        return (secs <= 0) ? "none" : calculateTime(secs);
     }
+
+    private static String calculateTime(int secs) {
+        Map<String, Integer> timeByUnit = calculateTimeByUnit(secs);
+        StringBuffer timeText = new StringBuffer();
+        int size = timeByUnit.size();
+        AtomicInteger count = new AtomicInteger(1);
+        timeByUnit.forEach((unit, value) -> {
+            String units = (value > 1) ? unit + "s" : unit;
+            String separator = isLastSeparator(size, count.get()) ? ", " : " and ";
+            if (hasMoreValues(size, count.getAndIncrement())) {
+                timeText.append(value);
+                timeText.append(" ");
+                timeText.append(units);
+                timeText.append(separator);
+            } else {
+                timeText.append(value);
+                timeText.append(" ");
+                timeText.append(units);
+            }
+        });
+        return timeText.toString();
+    }
+
+    private static boolean hasMoreValues(int size, int count) {
+        return count != size;
+    }
+
+    private static boolean isLastSeparator(int size, int count) {
+        return count + 1 != size;
+    }
+
+    private static Map calculateTimeByUnit(int secs) {
+        LinkedHashMap timeByUnit = new LinkedHashMap<String, Integer>();
+        Integer years = getUnitOfTime(secs, UnitOfTime.year.secs);
+        if (years > 0) {
+            timeByUnit.put(UnitOfTime.year.name(),years);
+        }
+        Integer days = getRemainingDays(secs);
+        if (days > 0) {
+            timeByUnit.put(UnitOfTime.day.name(), days);
+        }
+        Integer hours = getRemainingHours(secs);
+        if (hours > 0) {
+            timeByUnit.put(UnitOfTime.hour.name(), hours);
+        }
+        Integer mins = getRemainingMins(secs);
+        if (mins > 0) {
+            timeByUnit.put(UnitOfTime.minute.name(), mins);
+        }
+        Integer seconds = getRemainingSecs(secs);
+        if (seconds > 0) {
+            timeByUnit.put(UnitOfTime.second.name(), seconds);
+        }
+        return timeByUnit;
+    }
+
+    private static Integer getRemainingDays(int secs) {
+        return getUnitOfTime(secs, UnitOfTime.day.secs) - (365 * getUnitOfTime(secs, UnitOfTime.year.secs));
+    }
+
+
+    private static Integer getRemainingSecs(int secs) {
+        return secs % UnitOfTime.minute.secs;
+    }
+
+    private static Integer getRemainingMins(int secs) {
+        return getUnitOfTime(secs, UnitOfTime.minute.secs) - (UnitOfTime.minute.secs * getUnitOfTime(secs, UnitOfTime.hour.secs));
+    }
+
+    private static Integer getRemainingHours(int secs) {
+        return getUnitOfTime(secs, UnitOfTime.hour.secs) - (24 * getUnitOfTime(secs, UnitOfTime.day.secs));
+    }
+
+    private static Integer getUnitOfTime(int secs, int units) {
+        return secs / units;
+    }
+
 
 }
--- src/test/groovy/FormatTimeSpec.groovy
+++ src/test/groovy/FormatTimeSpec.groovy
@@ -4,6 +4,15 @@ import spock.lang.Unroll
 
 class FormatTimeSpec extends Specification {
 
+
+    /**
+
+     *
+     * test('negative seconds', () => {
+     *   expect(formatTime(-1)).toBe('none');
+     * });
+     * @return
+     */
     @Unroll
     def "time of '#timeInSecs' returns '#expectedTime'"() {
 
@@ -14,6 +23,22 @@ class FormatTimeSpec extends Specification {
                 timeInSecs  | expectedTime
                 0           |"none"
                 2           |"2 seconds"
+                62          |"1 minute and 2 seconds"
+                122         |"2 minutes and 2 seconds"
+                120         |"2 minutes"
+                3662        |"1 hour, 1 minute and 2 seconds"
+                7262        |"2 hours, 1 minute and 2 seconds"
+                7260        |"2 hours and 1 minute"
+                7201        |"2 hours and 1 second"
+                86400       |"1 day"
+                86401       |"1 day and 1 second"
+                86461       |"1 day, 1 minute and 1 second"
+                90061       |"1 day, 1 hour, 1 minute and 1 second"
+                31536000    |"1 year"
+                62985600    |"1 year and 364 days"
+                62985539    |"1 year, 363 days, 23 hours, 58 minutes and 59 seconds"
+                -1          |"none"
+        //TODO non numeric
     }
 
 
